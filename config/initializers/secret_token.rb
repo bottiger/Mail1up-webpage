6ddb407fe311e0c27880f98dbd926840dcdbde93
Arvid Bttiger <bottiger@gmail.com>
2012-01-09 13:07:04 +0100

    Reset project and moved old stub frontpage to /public

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mail1up::Application.config.secret_token = '58f2b5dcb42012ad38f7a1c98d905a88483340cbf391b97b4ad9cf2bd1fa6610307d28c252c50d23f2a240465c973775068970325eace71a84d65d7151b48dd8'
