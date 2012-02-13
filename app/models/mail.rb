7921ba18926d9752920eb8c16b353b64a79c84c7
Arvid Bttiger <bottiger@gmail.com>
2012-02-13 13:32:48 +0100

    Added initialisation of the users backup dir

--- app/models/mail.rb
+++ app/models/mail.rb
@@ -1,2 +1,24 @@
 class Mail < ActiveRecord::Base
+
+  def self.consumer_key
+    ""
+  end
+
+  def self.consumer_secret
+    ""
+  end
+
+  def write_config_files(auth_file, mbsync_config)
+    dir_name = '/media/s3ql/' + self.mail
+    Dir::mkdir(dir_name) unless FileTest::directory?(dir_name)
+    Dir::mkdir(dir_name + '/Mail')
+    File.open(dir_name + '/xoauth.sh', "w+") do |f|
+      f.write(auth_file)
+      f.chmod(0755)
+    end
+    File.open(dir_name + '/mbsync.config', "w+") do |f|
+      f.write(mbsync_config)
+      f.chmod(0755)
+    end
+  end
 end
